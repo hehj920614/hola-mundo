fcac6cbba0424ee0c9cbfabc16d7eb11f1e3eb49
Jessica <99156023+hehj920614@users.noreply.github.com>
2022-02-06 21:45:28 -0600

    subir archivo local

---
+++ Persona.java
@@ -0,0 +1,34 @@
+package com.ceteq.model;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private Integer edad;
+	private String RFC;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public String getRFC() {
+		return RFC;
+	}
+	public void setRFC(String rFC) {
+		RFC = rFC;
+	}
+	
+	
+}
